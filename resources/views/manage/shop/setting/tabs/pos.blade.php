c22a2f9d700a2f016467b34da0b605531f6018a9
rimtaray <rimtaray@gmail.com>
2020-04-28 21:15:22 +0700

    เพิ่ม tap pos

--- resources/views/manage/shop/setting/tabs/pos.blade.php
+++ resources/views/manage/shop/setting/tabs/pos.blade.php
@@ -1,4 +1,40 @@
-html
+<div class="row">
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-body">
+
+                <h4 class="mt-0 header-title">ใบเสร็จ</h4>
+
+                    <div class="form-group">
+                        <label>รูปแบบใบเสร็จ</label>
+                        <div>
+                            <input type="text" class="form-control"
+                                    data-parsley-minlength="6" value="{{ LKS::txt_receipt($shop->receipt_type) }}" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>จำนวนใบเสร็จ</label>
+                        <div>
+                            <input type="text" class="form-control" required
+                                    data-parsley-maxlength="6" value="{{ $shop->receipt_number }}"/>
+                        </div>
+                    </div>
+
+                    <div class="form-group mb-0">
+                        <div>
+                            <button type="submit" class="btn btn-warning waves-effect waves-light mr-1">
+                                แก้ไข
+                            </button>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+    
+
+
+</div>
 
 @section('js')
 @parent
--- resources/views/manage/sidebar_shop_manage.blade.php
+++ resources/views/manage/sidebar_shop_manage.blade.php
@@ -7,24 +7,19 @@
                 <!-- Left Menu Start -->
                
                 <ul class="metismenu" id="side-menu">
-<<<<<<< HEAD
-<<<<<<< HEAD
+                    
                     <li class="menu-title"><?php echo $shop->name;?></li>
-=======
-=======
->>>>>>> 851ffbe4614f4c7e873f2429867d0c5e21fe7453
+                    
                     <?php if(isset($shop)):?>
                     <li class="p-2">
                         <button type="button" onclick="location.href='<?php echo url('');?>/shops'" class="btn btn-sm btn-primary btn-block">ร้านของฉัน</button>
                     </li>
                     <?php endif;?>
-<<<<<<< HEAD
->>>>>>> b4a5fa150ef9258aa2003564f845bf921b8ab1b8
+                    
                     <li class="menu-title">จัดการ</li>
-=======
+                    
                     <li class="menu-title"><?php echo $shop->name;?></li>
                     <!-- <li class="menu-title">จัดการ</li> -->
->>>>>>> 851ffbe4614f4c7e873f2429867d0c5e21fe7453
                     <li>
                         <a href="<?php echo url($shop->url);?>" class="waves-effect">
                             <i class="ti-home"></i> <span> <?php echo __('menu.dashboard');?>  </span>
@@ -36,15 +31,8 @@
                         </a>
                     </li>
                     <li>
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <a href="<?php echo url($shop->url.'/setting_shop');?>" class="waves-effect">
-=======
                         <a href="<?php echo url($shop->url.'/settings');?>" class="waves-effect">
->>>>>>> b4a5fa150ef9258aa2003564f845bf921b8ab1b8
-=======
-                        <a href="<?php echo url($shop->url.'/settings');?>" class="waves-effect">
->>>>>>> 851ffbe4614f4c7e873f2429867d0c5e21fe7453
                             <i class="ti-home"></i> <span> <?php echo __('menu.setup');?> </span>
                         </a>
                     </li>
