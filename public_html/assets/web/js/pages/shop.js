fdd4a14f4dd34ed899956bb6471007cd1679bf97
metha <botlaster@gmail.com>
2020-07-24 18:54:04 +0700

    shop_url jquery wrong ajax

--- public_html/assets/web/js/pages/shop.js
+++ public_html/assets/web/js/pages/shop.js
@@ -12,7 +12,8 @@ function get_product(){
     obj.search_product = $("#search_product").val();
     console.log(obj);
     var post=new JPost('');
-    post.url=location.origin+'/'+$("#shop_url").val()+'/get/prooduct/json';
+    var url = $("#shop_url").val()+'/get/prooduct/json';
+    post.url=url;
     // post.url=page.attr("next_page_url");
     post.success=function(r){
         if(r.result==0)
